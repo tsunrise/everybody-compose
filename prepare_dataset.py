f29a0654055b68a786aa77c1583980f480d6079f
Conghao Shen <me@tomshen.io>
2022-10-22 16:18:25 -0700

    feat: add `save_freq`

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -7,12 +7,13 @@ if __name__ == "__main__":
     parser.add_argument('--seq_len', type=int, default=64)
     parser.add_argument('--mono', type=bool, default=True)
     parser.add_argument('--max_files', type=int, default=-1)
+    parser.add_argument('--save-freq', type=int, default=500)
     args = parser.parse_args()
     
     seq_len = args.seq_len
     mono = args.mono
     max_files = None if args.max_files == -1 else args.max_files
-    X, y = prepare_dataset(args.seq_len, mono, max_files, progress_save_freq=10)
+    X, y = prepare_dataset(args.seq_len, mono, max_files, progress_save_freq=args.save_freq)
     print(f"X.shape={X.shape}")
     print(f"y.shape={y.shape}")
     
