d43bb75a4633b57005e601cf5e13d02e4da6ed51
YixinLiu030 <liuyixin0111@hotmail.com>
2022-11-17 18:19:35 -0800

    update vanilla rnn architecture

--- models/vanilla_rnn.py
+++ models/vanilla_rnn.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from models.model_utils import ConcatPrev
 
 
 class DeepBeats_VanillaRNN(nn.Module):
@@ -8,9 +9,14 @@ class DeepBeats_VanillaRNN(nn.Module):
         super(DeepBeats_VanillaRNN, self).__init__()
         self.num_notes = num_notes
         self.note_embedding = nn.Embedding(num_notes, embed_size)
+        self.concat_prev = ConcatPrev()
+        self.concat_input_fc = nn.Linear(embed_size + 2, embed_size + 2)
+        self.concat_input_activation = nn.LeakyReLU()
         self.layer1 = nn.RNN(embed_size + 2, hidden_dim, batch_first=True)
         self.layer2 = nn.RNN(hidden_dim, hidden_dim, batch_first=True)
         self.notes_output = nn.Linear(hidden_dim, num_notes)
+
+        self._initializer_weights()
 
     def _default_init_hidden(self, batch_size):
         device = next(self.parameters()).device
@@ -18,13 +24,24 @@ class DeepBeats_VanillaRNN(nn.Module):
         h2_0 = torch.zeros(1, batch_size, self.layer2.hidden_size).to(device)
         return h1_0, h2_0
 
+    def _initializer_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
     def forward(self, x, y_prev, init_hidden = None):
         h1_0, h2_0 = self._default_init_hidden(x.shape[0]) if init_hidden is None else init_hidden
         y_prev_embed = self.note_embedding(y_prev)
-        x = torch.cat((x, y_prev_embed), dim=2)
-        x, h1 = self.layer1(x, h1_0)
-        x, h2 = self.layer2(x, h2_0)
-        predicted_notes = self.notes_output(x)
+        X = self.concat_prev(x, y_prev_embed)
+        # Concat input
+        X_fc = self.concat_input_fc(X)
+        X_fc = self.concat_input_activation(X_fc)
+        # residual connection
+        X = X_fc + X
+        X, h1 = self.layer1(X, h1_0)
+        X, h2 = self.layer2(X, h2_0)
+        predicted_notes = self.notes_output(X)
         return predicted_notes, (h1, h2)
 
     def sample(self, x, y_init=0, temperature=1.0):
@@ -37,9 +54,15 @@ class DeepBeats_VanillaRNN(nn.Module):
             scores = scores.squeeze(0)
             scores = scores / temperature
             scores = torch.nn.functional.softmax(scores, dim=1)
-            y = torch.multinomial(scores, 1)
-            ys.append(y)
-        out = [y.item() for y in ys]
+            y_next = None
+            while y_next is None or y_next == ys[-1]:
+                top10 = torch.topk(scores, 3, dim=1)
+                indices, probs = top10.indices, top10.values
+                probs = probs / torch.sum(probs)
+                probs_idx = torch.multinomial(probs, 1)
+                y_next = indices[0, probs_idx]
+            ys.append(y_next)
+        out = [y.item() for y in ys[1:]]
         return np.array(out)
 
     def loss_function(self, pred, target):
