11c9a344d144cbddd957105d940399001e288222
Conghao Shen <me@tomshen.io>
2022-11-25 22:43:03 -0800

    feat: support multi-key import

--- create_beats.py
+++ create_beats.py
@@ -3,6 +3,7 @@ import numpy as np
 
 if __name__ == "__main__":
     beat_sequence = create_beat()
+    np.set_printoptions(precision=4)
     print(beat_sequence)
     # save the beat sequence to a file
     np.save("beat_sequence.npy", beat_sequence)
