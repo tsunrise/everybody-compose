fix: reshape pred and target in cross entropy loss computation (#49)
reshape pred and target in crossentropy

models/lstm_local_attn.py
@@ -103,8 +103,9 @@ class DeepBeatsLSTMLocalAttn(nn.Module):
         Target: (batch_size, seq_len), range from 0 to num_notes-1
         """
         criterion = nn.CrossEntropyLoss()
-        target_one_hot = torch.nn.functional.one_hot(target, self.num_notes).float()
-        loss = criterion(pred, target_one_hot)
+        target = target.flatten() # (batch_size * seq_len)
+        pred = pred.reshape(-1, pred.shape[-1]) # (batch_size * seq_len, num_notes)
+        loss = criterion(pred, target)
         return loss
 
     def clip_gradients_(self, max_value):