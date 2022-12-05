dc7e1ce267b889e5797e38c4e76f2e11a98fc170
Tom Shen <me@tomshen.io>
2022-12-04 21:27:10 -0800

    Hybrid Beam Search (#56)
    * feat: beam search
    
    * feat: stochastic beam search
    
    * fix: strategy
    
    * fix: better repeat_decay

--- utils/sample.py
+++ utils/sample.py
@@ -8,7 +8,7 @@ from models.attention_rnn import DeepBeatsAttentionRNN
 from models.lstm_local_attn import DeepBeatsLSTMLocalAttn
 from models.vanilla_rnn import DeepBeatsVanillaRNN
 from utils.distribution import DistributionGenerator, LSTMDistribution, TransformerDistribution, LocalAttnLSTMDistribution, AttentionRNNDistribution, VanillaRNNDistribution
-
+from tqdm import tqdm
 
 def get_distribution_generator(model, beats, device) -> DistributionGenerator:
     """
@@ -85,7 +85,8 @@ def stochastic_search(model, beats: np.ndarray, hint: List[int], device: str, to
     state = dist.initial_state(hint)
     generated_sequence = hint[:]
     prev_note = generated_sequence[-1]
-    for _ in range(beats.shape[0] - len(hint)):
+    progress_bar = tqdm(range(beats.shape[0] - len(hint)), desc="Stochastic search")
+    for _ in progress_bar:
         # get the distribution
         state, distribution = dist.proceed(state, prev_note)
         # sample
@@ -93,6 +94,56 @@ def stochastic_search(model, beats: np.ndarray, hint: List[int], device: str, to
         generated_sequence.append(sampled_note)
         prev_note = sampled_note
     return np.array(generated_sequence)
+
+def beam_search(model, beats: np.ndarray, hint: List[int], device: str, repeat_decay: float = 0.5, num_beams: int = 3, beam_prob: float = 0.7, temperature=1.) -> np.ndarray:
+    """
+    - `model`: model to use for sampling
+    - `seq_len`: the length of the sequence to be sampled
+    - `device`: the device to use
+    - `repeat_decay`: penalty on repeating the same note. Each time the same note is repeated, the probability of repeating it is multiplied by `1 - repeat_decay`.
+                      the probability of getting N repeats is upper bounded by `(1 - repeat_decay) ** N`
+    - `initial_note`: the "sequence-start" placeholder.
+    - `num_beams`: number of beams to use
+
+    Returns:
+    - `generated_sequence`: a numpy array of shape (seq_len, ), containing the generated sequence
+    """
+    dist = get_distribution_generator(model, beats, device)
+    state = dist.initial_state(hint)
+    beams = [(hint[:], state, 0)] # (generated_sequence, state, log_likelihood)
+    progress_bar = tqdm(range(beats.shape[0] - len(hint)), desc="Beam search")
+    for _ in progress_bar:
+        beam_choice = np.random.rand()
+        if beam_choice < beam_prob:
+            new_beams = []
+            for beam in beams:
+                prev_note = beam[0][-1]
+                state, distribution = dist.proceed(beam[1], prev_note)
+                # modify the distribution using the repeat_decay
+                distribution[prev_note] *= (1 - repeat_decay)
+                # sample
+                for sampled_note in range(128):
+                    new_beam = (beam[0] + [sampled_note], state, beam[2] + np.log(distribution[sampled_note].item()))
+                    new_beams.append(new_beam)
+            # sort the beams by their likelihood
+            new_beams = sorted(new_beams, key=lambda x: x[2], reverse=True)
+            # keep only the top num_beams
+            beams = new_beams[:num_beams]
+        else:
+            new_beams = []
+            for beam in beams:
+                prev_note = beam[0][-1]
+                state, distribution = dist.proceed(beam[1], prev_note)
+                # sample
+                sampled_note, conditional_likelihood = stochastic_step(prev_note, distribution, 1.0, 128, repeat_decay, temperature)
+                new_beam = (beam[0] + [sampled_note], state, beam[2] + np.log(conditional_likelihood))
+                new_beams.append(new_beam)
+            # sort the beams by their likelihood
+            new_beams = sorted(new_beams, key=lambda x: x[2], reverse=True)
+            # keep only the top num_beams
+            beams = new_beams[:num_beams]
+    # return the beam with the highest likelihood
+    return np.array(beams[0][0])
     
 
 
